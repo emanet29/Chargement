1576a013e1d06dba332a51133539542c543fb382
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-21 11:03:33 +0200

    interface

---
+++ Chargement/CustomLoadingView.swift
@@ -0,0 +1,24 @@
+//
+//  CustomLoadingView.swift
+//  Chargement
+//
+//  Created by Snoopy on 21/08/2018.
+//  Copyright © 2018 EMANET. All rights reserved.
+//
+
+import UIKit
+
+class CustomLoadingView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+
+}
