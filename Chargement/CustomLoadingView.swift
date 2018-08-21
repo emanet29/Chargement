d2093ad40c6a0673f28c11e347369372af658bc9
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-21 11:17:47 +0200

    cercle en CAShapeLayer

--- Chargement/CustomLoadingView.swift
+++ Chargement/CustomLoadingView.swift
@@ -9,16 +9,45 @@
 import UIKit
 
 class CustomLoadingView: UIView {
-
+    
+    var shapeLayer: CAShapeLayer?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .clear
     }
-    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        backgroundColor = .clear
+    }
+    
+    func cercle() -> UIBezierPath {
+        let rect = CGRect(x: 5, y: 5, width: frame.width - 10, height: frame.height - 10)
+        return UIBezierPath(ovalIn: rect)
+    }
+    
+    /*override func draw(_ rect: CGRect) {
+        let path = cercle()
+        UIColor.clear.setFill()
+        path.fill()
+        
+        UIColor.black.setStroke()
+        path.stroke()
+        
+    }*/
+    
+    func initialiserShape() {
+        shapeLayer = CAShapeLayer()
+        shapeLayer?.path = cercle().cgPath
+        shapeLayer?.backgroundColor = UIColor.clear.cgColor
+        shapeLayer?.fillColor = nil
+        shapeLayer?.strokeColor = UIColor.black.cgColor
+        shapeLayer?.lineWidth = 5
+        
+        layer.addSublayer(shapeLayer!)
+        
     }
     
     
-
 }
--- Chargement/ViewController.swift
+++ Chargement/ViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var customLoadingView: CustomLoadingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
     }
-
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        customLoadingView.initialiserShape()
+    }
     
 
 
